40a09ef2e651c0a4d886ec7f0c8dff030a78e672
Goutham Reddy <64308772+KGouthamReddy@users.noreply.github.com>
2020-10-13 08:02:54 +0530

    Creating a mini project
    A game using if-else condition through React JS.

---
+++ Slot.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import SlotM from './Slotmachine'
+
+
+
+const App = () => {
+return <>
+<h1 className="heading_style"> 
+🎰 Welcome to{""}<span
+style={{ fontWeight: "bold"}}> Slot Machine Game</span>🎰
+</h1>
+<div className="slotmachine">
+<SlotM x='😂' y='😂'z="😂"/>
+<SlotM  x='🏏' y='🍌'z="😁"/>
+<SlotM  x='🧑‍🤝‍🧑' y='💌'z="🍍"/>
+<SlotM  x='🧑‍🤝‍' y='😃'z="🍍"/>
+<SlotM  x='🧑‍🤝‍🧑' y='🧑‍🤝‍🧑'z="🧑‍🤝‍🧑"/>
+
+</div>
+</>
+}
+export default App;
